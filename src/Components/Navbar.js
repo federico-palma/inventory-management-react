779f7e62b15578af586f2a690bd1130e07a436e2
federico-palma <fedepal9@gmail.com>
2022-04-08 13:23:00 +0200

    Added navbar component

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div id="logo-container">
+                <h1>PIM</h1>
+                <h3>Palma Inventory <br></br> Management</h3>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
